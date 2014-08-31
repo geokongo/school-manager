1f0ee358ba233ab494c5b25a8a74b1f2e66fb270
GeoffreyOliver <geoffreyokongo2@gmail.com>
2014-08-31 09:05:23 -0700

    Updated .htsccess and config files

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/codeigniter';
+$config['base_url']	= 'http://localhost/schoolmanager';
 
 /*
 |--------------------------------------------------------------------------
