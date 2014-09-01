295ce319853b7010e9021e0011a0fbafc2486446
GeoffreyOliver <geoffreyokongo2@gmail.com>
2014-09-01 14:26:56 -0700

    Finalized the InstallScript

--- application/config/routes.php
+++ application/config/routes.php
@@ -25,7 +25,7 @@
 |
 | There area two reserved routes:
 |
-|	$route['default_controller'] = 'welcome';
+$route['default_controller'] = "login";
 |
 | This route indicates which controller class should be loaded if the
 | URI contains no data. In the above example, the "welcome" class
