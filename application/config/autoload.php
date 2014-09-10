0d92cb837b48148bed7c8a48b6a12731506d654c
GeoffreyOliver <geoffreyokongo2@gmail.com>
2014-09-10 10:05:18 -0700

    Unstaging database configuraton files

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -51,7 +51,7 @@ $autoload['packages'] = array();
 |
 */
 
-$autoload['libraries'] = array('database', 'form_validation', 'session', 'javascript' );
+$autoload['libraries'] = array('form_validation', 'session', 'javascript' );
 
 
 /*
